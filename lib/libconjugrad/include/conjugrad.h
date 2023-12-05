ab79723de855b93209cfeec8f6624e05948fcfa4
Phill Stansfeld <pstansfeld@gmail.com>
2023-12-05 14:06:16 +0000

    Update conjugrad.h

--- lib/libconjugrad/include/conjugrad.h
+++ lib/libconjugrad/include/conjugrad.h
@@ -17,7 +17,7 @@ typedef float conjugrad_float_t;
 #define F08  0.8f
 #define F1   1.0f
 #define F2   2.0f
-#define FInf 0.0f/0.0f
+//#define FInf 0.0f/0.0f
 #define fsqrt sqrtf
 #define flog logf
 #define fexp expf
